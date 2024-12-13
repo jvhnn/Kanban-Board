768cee7b328ef38e9ec556fa9c27d6d31372cc83
John Marquez <johnmarquez249@gmail.com>
2024-12-12 21:18:39 -0700

    concurrently to dependecies

--- server/dist/server.js
+++ server/dist/server.js
@@ -1,3 +1,8 @@
+import { fileURLToPath } from 'node:url';
+import path from 'path';
+// import seedAll from './seeds/index.js';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const forceDatabaseRefresh = false;
 import dotenv from 'dotenv';
 dotenv.config();
@@ -10,6 +15,13 @@ const PORT = process.env.PORT || 3001;
 app.use(express.static('../client/dist'));
 app.use(express.json());
 app.use(routes);
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.get('*', (__req, res) => {
+        res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+    });
+}
+// await seedAll();
 sequelize.sync({ force: forceDatabaseRefresh }).then(() => {
     app.listen(PORT, () => {
         console.log(`Server is listening on port ${PORT}`);
